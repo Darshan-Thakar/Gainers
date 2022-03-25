675e35feb21ca0e7362231c780c314541a5f9344
Darshan Vilas Thakar <santoshrthakar@gmail.com>
2022-03-25 16:59:04 +0530

    Modified the design of navbar

--- DigiKnowledge__frontend/src/App.js
+++ DigiKnowledge__frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Link,Route} from "react-router-dom"
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
+import Navbar from './components/Navbar'
 function App(props) {
 
 
@@ -19,6 +20,7 @@ function App(props) {
 </Route>
  
  <Route exact path="/home">
+   <Navbar/>
 <Home/>
  </Route>
 </Switch>
--- DigiKnowledge__frontend/src/components/Home.jsx
+++ DigiKnowledge__frontend/src/components/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import "../components__Styling/Home.css"
-import Navbar from './Navbar'
+
 function Home() {
     return (
         <div className="home">
-            <nav className="navbar">
-                <Navbar/>
-            </nav>
+         
+
         </div>
     )
 }
