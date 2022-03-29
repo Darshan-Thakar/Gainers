2f0322cfcf5b15ea4fd7e45b971911dceb4e7d59
Darshan Vilas Thakar <santoshrthakar@gmail.com>
2022-03-29 20:43:17 +0530

    Added few required features for managing user state

--- DigiKnowledge__frontend/src/context/UserState.js
+++ DigiKnowledge__frontend/src/context/UserState.js
@@ -1,12 +1,30 @@
 import UserContext from "./UserContext";
 import { useState } from "react";
 
+
 const UserState=(props)=>{
 const host="http://localhost:8000"
+const  [User, setUser] = useState({});
+const [Loading, setLoading] = useState(true);
+
+const getCurrentUser=async()=>{
+    setLoading(true);
+    const response=await fetch(`${host}/auth/getUser`,{
+        headers:{
+           "Auth-Token":localStorage.getItem("token")
+        },
+
+    })
+
+    const parsedResponse=await response.json();
+console.log(parsedResponse)
+    setUser(parsedResponse);
+    setLoading(false);
+}
 
 
     return(
-<UserContext.Provider value={ {hello:"hello"}}>
+<UserContext.Provider value={{user:User, getCurrentUser:getCurrentUser,loading:Loading}}>
        { props.children}
  </UserContext.Provider>
 
