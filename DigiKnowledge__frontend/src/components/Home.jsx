10a9bfb5c7f17297d996100aa804829d6979ee13
Darshan Vilas Thakar <santoshrthakar@gmail.com>
2022-03-29 20:42:02 +0530

    Added few design modifications

--- DigiKnowledge__frontend/src/components/Home.jsx
+++ DigiKnowledge__frontend/src/components/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import "../components__Styling/Home.css"
 import ProfileCard from './ProfileCard'
+import {useHistory} from "react-router-dom"
 function Home() {
+    const history=useHistory();
     return (
         <div className="home">
+            {localStorage.getItem("token")?
+            <>
          <div  id="left__side">
             <ProfileCard/>
          </div>
          <div id="middle__side">
 
          </div>
-         
-
+         </>
+:   history.push("/login") }
         </div>
     )
 }
--- DigiKnowledge__frontend/src/index.js
+++ DigiKnowledge__frontend/src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import UserState from './context/UserState';
 import reportWebVitals from './reportWebVitals';
 
 
 
  ReactDOM.render(
 
-    
+<UserState>
      <App /> 
-
-
+     </UserState>
 ,
   document.getElementById('root')
  );
